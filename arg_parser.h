1a10dabca66934f0689787d75733a6522f804552
azinum <lukaska61@gmail.com>
2023-02-04 20:58:39 +0100

    proper typedefs of integer types

--- arg_parser.h
+++ arg_parser.h
@@ -14,10 +14,10 @@
 
 typedef int32_t i32;
 typedef uint32_t u32;
-typedef int32_t i16;
-typedef uint32_t u16;
-typedef int32_t i8;
-typedef uint32_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int8_t i8;
+typedef uint8_t u8;
 typedef float f32;
 
 #define ARRAY_LENGTH(ARR) (sizeof(ARR) / sizeof(ARR[0]))
